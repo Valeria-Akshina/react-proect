85e55c73f85efde515217187c9a5c7ae233f236e
Valeria-Akshina <aksinalera@gmail.com>
2025-12-04 05:29:20 +0300

    adaptiv header

--- shann/src/components/Header/index.jsx
+++ shann/src/components/Header/index.jsx
@@ -1,17 +1,22 @@
 import style from './style/index.module.css'
 import logo from '../../assets/images/logo.png'
+import phone from '../../assets/icon/phone.png'
 
 
 export const Header = () => {
     return (
             <header className={style.header}>
         <div className={style.header__logo}>
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="#" />
         </div>
         <div className={style.header__block}>
             <p className={style.block__phone}>8 800 555 80 80</p>
             <button className={style.block__btn__blue}>Заказать звонок</button>
         </div>
+
+        <button className={style.mobile__phone__btn}>
+            <img src={phone} alt="#" />
+        </button>
     </header>
     )
 } 
